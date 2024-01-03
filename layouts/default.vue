f1506688de547cdc5e75e77dfa79f69d5584237e
mehaac <meha6699@gmail.com>
2024-01-03 12:36:53 +0300

    feat: Card

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,22 +4,44 @@ import { Input } from '@/components/ui/input'
 </script>
 
 <template>
-  <header class="px-6 py-4 flex flex-col gap-2">
-    <div class="flex items-center gap-4 py-2">
-      <div class="logo">
-        LOGO
+  <div class="h-[100dvh] overflow-hidden relative flex flex-col">
+    <header class="px-6 py-4 flex flex-col gap-2 bg-background">
+      <div class="flex items-center gap-4 py-2">
+        <div class="logo">
+          LOGO
+        </div>
+        <div class="search w-full">
+          <Input type="text" placeholder="Search..." />
+        </div>
+        <DarkToggle />
+
+        <div class="avatar">
+          AVATAR
+        </div>
       </div>
-      <div class="search w-full">
-        <Input type="text" placeholder="Search..." />
+      <UiSeparator orientation="horizontal" />
+    </header>
+    <main class="flex gap-2 flex-col overflow-y-auto px-6 relative">
+      <div class="flex gap-4 pb-4 sticky top-0 bg-background z-10">
+        <div v-for="n in 7" :key="n" class="bg-secondary border-[1px] px-4 py-1 rounded-lg border-primary">
+          tag{{ n }}
+        </div>
       </div>
-      <DarkToggle />
-      <div class="avatar">
-        AVATAR
+      <slot />
+    </main>
+    <footer class="px-6 pt-4 flex flex-col gap-2 bg-background">
+      <UiSeparator orientation="horizontal" />
+      <div class="flex justify-between h-12">
+        <div class="flex flex-col">
+          <div>
+            1
+          </div>
+          <div>2</div>
+        </div>
+        <div>
+          1
+        </div>
       </div>
-    </div>
-    <UiSeparator orientation="horizontal" />
-  </header>
-  <main class="pt-4">
-    <slot />
-  </main>
+    </footer>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <script lang="ts" setup>
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 definePageMeta({
   middleware: ['protected'],
   layout: 'default',
@@ -18,11 +27,23 @@ async function handleLogout(e: Event) {
 </script>
 
 <template>
-  <div>
-    <h1>Profile</h1>
-    <p>GitHub username: {{ user.githubUsername }}</p>
-    <form method="post" action="/api/logout" @submit.prevent="handleLogout">
-      <input type="submit" value="Sign out">
-    </form>
+  <div class="flex flex-col px-12 gap-2 w-full relative">
+    <Card v-for="n in 5" :key="n">
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        Card Content
+      </CardContent>
+      <CardFooter>
+        Card Footer
+      </CardFooter>
+    </Card>
+    <Card class="sticky bottom-0 left-0">
+      <CardContent class="flex items-center justify-center">
+        ADD TODO
+      </CardContent>
+    </Card>
   </div>
 </template>
