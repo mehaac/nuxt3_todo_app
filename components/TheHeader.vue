98214c84f5b184773cc91e1ab09f29a775879ad9
mehaac <meha6699@gmail.com>
2024-01-04 13:30:14 +0300

    (WIP)refactor: TheHeader GitHubUser

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,6 +2,7 @@
 import { Input } from '@/components/ui/input'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { UiSeparator } from '@/components/ui/separator'
+import { UiLarge } from '@/components/ui/typography'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,13 +16,26 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from '@/components/ui/tooltip'
+
+const user = useAuthenticatedUser()
+async function handleLogout(e: Event) {
+  if (!(e.target instanceof HTMLFormElement))
+    return
+  await $fetch('/api/logout', {
+    method: 'POST',
+    redirect: 'manual',
+  })
+  await navigateTo('/login')
+}
 </script>
 
 <template>
   <header class="px-6 py-4 flex flex-col gap-2 bg-background">
     <div class="flex items-center gap-4 py-2">
       <div class="logo">
-        LOGO
+        <UiLarge>
+          ToDo
+        </UiLarge>
       </div>
       <div class="search w-full">
         <Input type="text" placeholder="Search..." />
@@ -32,19 +46,23 @@ import {
           <Tooltip :delay-duration="0">
             <TooltipTrigger>
               <Avatar class="avatar">
-                <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-                <AvatarFallback>User</AvatarFallback>
+                <AvatarImage :src="`https://avatars.githubusercontent.com/${user.githubUsername}`" :alt="`github ${user.githubUsername} avatar`" />
+                <AvatarFallback>{{ user.githubUsername }}</AvatarFallback>
               </Avatar>
             </TooltipTrigger>
             <TooltipContent>
-              User
+              {{ user.githubUsername }}
             </TooltipContent>
           </Tooltip>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>User</DropdownMenuLabel>
+          <DropdownMenuLabel>{{ user.githubUsername }}</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>Logout</DropdownMenuItem>
+          <DropdownMenuItem as-child>
+            <form method="post" action="/api/logout" @submit.prevent="handleLogout">
+              <input type="submit" value="Sign out">
+            </form>
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
