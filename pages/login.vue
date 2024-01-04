8b99dc6adfc02c1bd3b3fb844879404b3f6b652c
mehaac <meha6699@gmail.com>
2024-01-04 13:28:06 +0300

    style: login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,28 @@
 <script lang="ts" setup>
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+
+definePageMeta({
+  layout: 'auth',
+})
 const user = await useUser()
 if (user.value)
   await navigateTo('/') // redirect to profile page
 </script>
 
 <template>
-  <DarkToggle />
-  <h1>Sign in</h1>
-  <a href="/api/login/github">Sign in with GitHub</a>
+  <div class="flex h-full justify-center items-center">
+    <Card class="w-[320px]">
+      <CardHeader class="text-center">
+        <CardTitle>
+          Sign in
+        </CardTitle>
+      </CardHeader>
+      <CardContent class="flex justify-center">
+        <Button as="a" href="/api/login/github">
+          Sign in with GitHub
+        </Button>
+      </CardContent>
+    </Card>
+  </div>
 </template>
