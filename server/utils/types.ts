e2bf24cf713cfad0c256a3e57283013f4fed4f02
mehaac <meha6699@gmail.com>
2024-01-10 12:32:21 +0300

    feat: task types

---
+++ server/utils/types.ts
@@ -0,0 +1,18 @@
+import z from 'zod'
+
+export const TaskStatus = ['todo', 'in-progress', 'done'] as const
+
+export const taskDtoSchema = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  text: z.string().min(4).max(255),
+  priority: z.number().min(0).max(5),
+  status: z.enum(TaskStatus),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
+})
+
+export const taskFormSchema = taskDtoSchema.omit({ id: true, userId: true })
+
+export type TaskDto = z.infer<typeof taskDtoSchema>
+export type TaskForm = z.infer<typeof taskFormSchema>
