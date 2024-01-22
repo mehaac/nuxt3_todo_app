4e6a6e215e45ccd38c6f17cca14d0e1c0d549c0e
mehaac <meha6699@gmail.com>
2024-01-22 13:14:38 +0300

    style: add loading animation

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -3,9 +3,6 @@ import { Button } from './ui/button'
 import { UiLarge } from './ui/typography'
 import {
   Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
 } from '@/components/ui/card'
 import { UiSeparator } from '@/components/ui/separator'
 import TaskFormPrioritySelect from '@/components/task/form/PrioritySelect.vue'
@@ -31,14 +28,17 @@ const formattedCreatedAt = useDateFormat(props.task.createdAt, 'Do MMMM HH:mm', 
 </script>
 
 <template>
-  <Card class="flex w-full max-w-xl" as="article">
-    <div class="flex items-center p-2 gap-2 shrink-0">
+  <Card class="flex w-full max-w-xl relative" as="article">
+    <div class="flex items-center justify-center p-2 shrink-0 ">
       <Checkbox @update:checked="emit('checked', { id: task.id })" />
+    </div>
+    <UiSeparator orientation="vertical" class="mx-2" />
+    <div class="flex items-center p-2 gap-2 shrink-0 w-48">
       <TaskFormPrioritySelect :disabled="isDeleting || isUpdating" />
       <TaskFormStatusSelect :disabled="isDeleting || isUpdating" />
     </div>
     <UiSeparator orientation="vertical" class="mx-2" />
-    <div class="flex grow">
+    <div class="flex grow items-center">
       <Button v-show="!isEdited" variant="link" class="min-h-full h-full" :disabled="isDeleting || isUpdating">
         <UiLarge class="break-all line-clamp-1" @click="toggleEdited">
           {{ task.text }}
@@ -52,53 +52,30 @@ const formattedCreatedAt = useDateFormat(props.task.createdAt, 'Do MMMM HH:mm', 
           Edit
         </Button>
       </div>
-    </div>
-    <div v-show="isUpdating">
-      <Icon name="carbon:progress-bar-round" size="32" class="animate-spin" />
-    </div>
-  </Card>
-  <Card
-    v-if="false" class="overflow-hidden flex flex-col max-w-xl w-full min-w-[320px]" as="article"
-  >
-    <CardHeader class="">
-      <div class="flex gap-4">
-        <div>
-          <TaskFormPrioritySelect :disabled="isDeleting || isUpdating" />
-        </div>
-        <div>
-          <TaskFormStatusSelect :disabled="isDeleting || isUpdating" />
-        </div>
-        <div class="ml-auto">
-          {{ formattedCreatedAt }}
-        </div>
-      </div>
-    </CardHeader>
-    <UiSeparator orientation="horizontal" class="mb-5" />
-    <CardContent class="flex flex-col gap-2  grow-[2]">
-      <Button v-show="!isEdited" variant="link" class="min-h-full h-full" :disabled="isDeleting || isUpdating">
-        <UiLarge class="break-all" @click="toggleEdited">
-          {{ task.text }}
-        </UiLarge>
-      </Button>
-      <div v-show="isEdited" class="flex flex-col gap-2">
-        <TaskFormTextarea />
-        <Button
-          variant="outline" @click="handleUpdateText"
+      <Transition name="loading">
+        <div
+          v-if="isUpdating"
+          class="absolute"
         >
-          Edit
-        </Button>
-      </div>
-    </CardContent>
-    <UiSeparator orientation="horizontal" class="mb-5 text-start" />
-    <CardFooter>
-      <div class="flex gap-4 items-center justify-between">
-        <Button variant="destructive" :disabled="isDeleting" @click="handleDelete">
-          Delete
-        </Button>
-        <div v-show="isUpdating">
-          <Icon name="carbon:progress-bar-round" size="32" class="animate-spin" />
+          <Icon
+            name="carbon:progress-bar-round"
+            size="32"
+            class="animate-spin"
+          />
         </div>
-      </div>
-    </CardFooter>
+      </Transition>
+    </div>
   </Card>
 </template>
+
+<style scoped>
+.loading-enter-active,
+.loading-leave-active {
+  transition: all 1s linear;
+  right: -2.5rem;
+}
+.loading-leave-to {
+  transform: translate3d(4rem, 0, 0);
+  opacity: 0;
+}
+</style>
